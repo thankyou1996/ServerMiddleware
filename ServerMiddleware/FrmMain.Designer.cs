9a61bfae22cf78c848e6f5c6b2ed5d189d3724c0
thankyou1996 <1296214557@qq.com>
2019-09-24 19:12:22 +0800

    添加项目文件。

---
+++ ServerMiddleware/FrmMain.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace ServerMiddleware
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(75, 30);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(139, 21);
+            this.txtAddress.TabIndex = 0;
+            this.txtAddress.Text = "192.168.2.18";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(220, 30);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(100, 21);
+            this.txtPort.TabIndex = 0;
+            this.txtPort.Text = "6800";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(335, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(75, 77);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(245, 21);
+            this.txtSend.TabIndex = 2;
+            this.txtSend.Text = "aabbcc";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(335, 77);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "发送";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 462);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtAddress);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ ServerMiddleware/FrmMain.cs
@@ -0,0 +1,144 @@
+﻿using HPSocketCS;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ServerMiddleware
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        HPSocketCS.TcpClient client = new HPSocketCS.TcpClient();
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            Init();
+        }
+        public void Init()
+        {
+            client.OnPrepareConnect += new TcpClientEvent.OnPrepareConnectEventHandler(OnPrepareConnect);
+            client.OnConnect += new TcpClientEvent.OnConnectEventHandler(OnConnect);
+            client.OnSend += new TcpClientEvent.OnSendEventHandler(OnSend);
+            client.OnReceive += new TcpClientEvent.OnReceiveEventHandler(OnReceive);
+            client.OnClose += new TcpClientEvent.OnCloseEventHandler(OnClose);
+        }
+
+        #region 初始化相关函数
+
+        HandleResult OnPrepareConnect(TcpClient sender, IntPtr socket)
+        {
+            return HandleResult.Ok;
+        }
+
+        HandleResult OnConnect(TcpClient sender)
+        {
+            // 已连接 到达一次
+
+            // 如果是异步联接,更新界面状态
+            //this.Invoke(new ConnectUpdateUiDelegate(ConnectUpdateUi));
+
+            AddMsg(string.Format(" > [{0},OnConnect]", sender.ConnectionId));
+
+            return HandleResult.Ok;
+        }
+
+        HandleResult OnSend(TcpClient sender, byte[] bytes)
+        {
+            // 客户端发数据了
+            AddMsg(string.Format(" > [{0},OnSend] -> ({1} bytes)", sender.ConnectionId, bytes.Length));
+
+            return HandleResult.Ok;
+        }
+
+        HandleResult OnReceive(TcpClient sender, byte[] bytes)
+        {
+
+            AddMsg(string.Format(" > [{0},OnReceive] -> ({1} bytes)", sender.ConnectionId, bytes.Length));
+            return HandleResult.Ok;
+        }
+
+        HandleResult OnClose(TcpClient sender, SocketOperation enOperation, int errorCode)
+        {
+            if (errorCode == 0)
+                // 连接关闭了
+                AddMsg(string.Format(" > [{0},OnClose]", sender.ConnectionId));
+            else
+                // 出错了
+                AddMsg(string.Format(" > [{0},OnError] -> OP:{1},CODE:{2}", sender.ConnectionId, enOperation, errorCode));
+
+            // 通知界面,只处理了连接错误,也没进行是不是连接错误的判断,所以有错误就会设置界面
+            // 生产环境请自己控制
+            //this.Invoke(new SetAppStateDelegate(SetAppState), AppState.Stoped);
+
+            return HandleResult.Ok;
+        }
+
+        /// <summary>
+        /// 往listbox加一条项目
+        /// </summary>
+        /// <param name="msg"></param>
+        void AddMsg(string msg)
+        {
+            Console.WriteLine(msg);
+
+            //if (this.lbxMsg.InvokeRequired)
+            //{
+            //    // 很帅的调自己
+            //    this.lbxMsg.Invoke(AddMsgDelegate, msg);
+            //}
+            //else
+            //{
+            //    if (this.lbxMsg.Items.Count > 100)
+            //    {
+            //        this.lbxMsg.Items.RemoveAt(0);
+            //    }
+            //    this.lbxMsg.Items.Add(msg);
+            //    this.lbxMsg.TopIndex = this.lbxMsg.Items.Count - (int)(this.lbxMsg.Height / this.lbxMsg.ItemHeight);
+            //}
+        }
+        #endregion
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string ip = txtAddress.Text;
+            ushort port = Convert.ToUInt16(txtPort.Text);
+            if (client.Connect(ip, port, false))
+            {
+                AddMsg("Conect OK");
+            }
+            else
+            {
+                AddMsg("Connect Fail");
+            }
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            string send = this.txtSend.Text;
+            if (send.Length == 0)
+            {
+                return;
+            }
+
+            byte[] bytes = Encoding.Default.GetBytes(send);
+            IntPtr connId = client.ConnectionId;
+
+            // 发送
+            if (client.Send(bytes, bytes.Length))
+            {
+                AddMsg(string.Format("$ ({0}) Send OK --> {1}", connId, send));
+            }
+            else
+            {
+                AddMsg(string.Format("$ ({0}) Send Fail --> {1} ({2})", connId, send, bytes.Length));
+            }
+        }
+    }
+}
